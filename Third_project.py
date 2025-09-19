29e57a92817ec77288149cd7cebbd40f4b70db8e
ARawat120 <123465419+ARawat120@users.noreply.github.com>
2025-09-19 13:37:36 +0530

    Third_project.py

--- Third_project.py
+++ Third_project.py
@@ -2,4 +2,27 @@
 
 # Rock-Paper-Scissor game
 
+import emoji
 import random
+def play_game():
+
+    print("Welcome to our childhood game")
+choices = ['rock','paper','scissor']
+while True:
+
+    user = input("Choose your prop (rock,paper,scissor): ",).lower()
+    if user == 'quit':
+        print("Thanks for Playing")
+        break
+    elif user not in  choices:
+        print("Invalid prop Enter right prop")
+        continue
+    computer = random.choice(choices)
+    print(f"computer choose : {computer}")
+    if user == computer:
+        print("Its tie \U0001F61B")
+    elif user == 'rock' and computer == 'paper' or user == 'scissor' and computer == 'paper'or user == 'rock' and computer == 'scissor':
+        print("Congratulation you win \U0001F389 ")
+    else:
+        print("ohh you loose \U0001F622")
+play_game()
